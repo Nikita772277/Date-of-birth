462b6a9a883e4971d791907d0f436bca31f6e260
Nikita77227 <Nikita77227@DESKTOP-029TR2H>
2022-10-25 19:31:00 +0300

    Добавьте файлы проекта.

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+while (true)
+{
+
+    Console.WriteLine("Введите дату рождения");
+    Console.WriteLine();
+    Console.WriteLine("Пример ввода - 24.9.1980");
+    Console.WriteLine();
+    string date = Console.ReadLine();
+    if (date == "" || date == " ")
+    {
+        Console.WriteLine($"Вы ввели некорректные данные");
+        Console.WriteLine();
+    }
+    else
+    {
+        Regex regex = new Regex(@"[1-30]{1,2}.[1-12,1-9]{1,2}.[1913-2017,1-9]{3,4}$");
+        regex.IsMatch(date);
+        if (regex.IsMatch(date))
+        {
+            Console.WriteLine($"Дата рождения принята");
+            Console.WriteLine();
+            Console.WriteLine($"Введённая дата рождения - {date}");
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели некорректные данные");
+        }
+    }
+}
